f493ce9a6e942c2f1661de5df87c6723a9d4cb0c
Farnaz Fooladi <50850823+farnazfooladi@users.noreply.github.com>
2021-12-17 13:39:51 -0800

    Time: 214 ms (36.56%), Space: 19.5 MB (99.85%) - LeetHub

--- 221-maximal-square/221-maximal-square.py
+++ 221-maximal-square/221-maximal-square.py
@@ -1,9 +1,11 @@
 class Solution:
     def maximalSquare(self, M):
         m, n, ans = len(M), len(M[0]), 0
-        dp = [[0]*(n+1) for i in range(m+1)]
-        
         for i, j in product(range(m-1, -1, -1), range(n-1, -1, -1)):
-            dp[i][j] = 1 + min(dp[i+1][j], dp[i][j+1], dp[i+1][j+1]) if M[i][j] == '1' else 0
-            ans = max(ans, dp[i][j])
+            if i == m-1 or j == n-1:
+                M[i][j] = int(M[i][j])
+                ans = max(ans, M[i][j])
+            else:
+                M[i][j] = 1 + min(M[i+1][j], M[i][j+1], M[i+1][j+1]) if M[i][j] == '1' else 0
+                ans = max(ans, M[i][j])
         return ans * ans
