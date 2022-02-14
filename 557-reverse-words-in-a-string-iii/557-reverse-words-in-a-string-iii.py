20ffef8031b3bd96c6ef1406077f08d36cbf0a86
Farnaz Fooladi <50850823+farnazfooladi@users.noreply.github.com>
2022-02-14 14:48:16 -0800

    Time: 29 ms (80.59%), Space: 14.4 MB (81.11%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -4,20 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        stringReverse = ''
-        left = 0
-        for i in range(len(s)):
-            if s[i] == ' ':
-                right = i-1
-                while left <= right:
-                    stringReverse += s[right]
-                    right -= 1
-                stringReverse += ' '
-                left = i+1
-            if i == len(s)-1:
-                right = i
-                while left <= right:
-                    stringReverse += s[right]
-                    right -= 1
-        return stringReverse
+        s = s.split()
+        l = len(s)
+        for i in range(l):
+            s[i] = s[i][::-1]
+        return ' '.join(s)
+        # stringReverse = ''
+        # left = 0
+        # for i in range(len(s)):
+        #     if s[i] == ' ':
+        #         right = i-1
+        #         while left <= right:
+        #             stringReverse += s[right]
+        #             right -= 1
+        #         stringReverse += ' '
+        #         left = i+1
+        #     if i == len(s)-1:
+        #         right = i
+        #         while left <= right:
+        #             stringReverse += s[right]
+        #             right -= 1
+        # return stringReverse
         
