3f1182b8f8ddcedde38d3725bf8314a090a6f1b7
Farnaz Fooladi <50850823+farnazfooladi@users.noreply.github.com>
2022-02-22 12:33:01 -0800

    Time: 124 ms (64.31%), Space: 17.6 MB (19.42%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,41 +1,81 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        test = [0] * numCourses
-        output = []
+        preMap = {}
+        for c in range(numCourses):
+            preMap[c] = []
         
-        prereq = {c:[] for c in range(numCourses)}
         for crs, pre in prerequisites:
-            prereq[crs].append(pre)
+            preMap[crs].append(pre)
             
-        counter = 0
+        output = []
+        visit, cycle = set(), set()
+        
+        def dfs(crs):
+            if crs in cycle: return False
+            if crs in visit: return True
+            
+            cycle.add(crs)
+            
+            for p in preMap[crs]:
+                if dfs(p) == False: return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
+            return True
+        
+        for c in range(numCourses):
+            if dfs(c) == False: return []
+        return output
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         test = [0] * numCourses
+#         output = []
+        
+#         prereq = {c:[] for c in range(numCourses)}
+#         for crs, pre in prerequisites:
+#             prereq[crs].append(pre)
+            
+#         counter = 0
                 
-        for i in prereq:
-            if not prereq[i]:
-                output.append(i)
-                test[i] = 1
-                counter += 1
+#         for i in prereq:
+#             if not prereq[i]:
+#                 output.append(i)
+#                 test[i] = 1
+#                 counter += 1
                 
-        size = len(prereq) - counter
+#         size = len(prereq) - counter
                 
         
         
-        for t in range(size):
-            for i in prereq:
-                if test[i] == 1:
-                    continue
-                x = True
-                for count in range(len(prereq[i])):
-                    if (test[prereq[i][count]] != 1):
-                        x = False
-                        break
-                if x:
-                    output.append(i)
-                    test[i] = 1
+#         for t in range(size):
+#             for i in prereq:
+#                 if test[i] == 1:
+#                     continue
+#                 x = True
+#                 for count in range(len(prereq[i])):
+#                     if (test[prereq[i][count]] != 1):
+#                         x = False
+#                         break
+#                 if x:
+#                     output.append(i)
+#                     test[i] = 1
         
-        if (len(output) == numCourses):
-            return output
+#         if (len(output) == numCourses):
+#             return output
             
             
-        return []
+#         return []
             
         
