fec468f26ff43e27d269b6a152a743d35cc28d77
Farnaz Fooladi <50850823+farnazfooladi@users.noreply.github.com>
2022-03-24 13:38:39 -0700

    Time: 27 ms (48.13%), Space: 13.5 MB (76.68%) - LeetHub

--- 68-text-justification/68-text-justification.py
+++ 68-text-justification/68-text-justification.py
@@ -5,38 +5,46 @@ class Solution(object):
         :type maxWidth: int
         :rtype: List[str]
         """
-        res = []
-        # number of words in the current line
-        count_words = 0
-        # number of non-space characters in the current line
-        count_chars = 0
-        # start index of the word list in the current line
-        start = 0
-        for i in range(len(words)):
-            # if current line ends with the previous words
-            # all words + space between them + the current word (no space with the previous word if any) reaches/exceeds the maxWidth of the current line, append the current line to the result and start a new line
-            if count_chars + count_words - 1 + len(words[i]) >= maxWidth:
-                # count the number of additional space character between words
-                space = maxWidth - count_chars - (count_words - 1)
-                # check whether the denominator is 0 (only one word in the current line)
-                if count_words == 1:
-                    res.append(words[start] + ' '*space)
-                # count number of additional space evenly distributed across words and the extra remainder of space for the first few words
-                else:
-                    sep = space // (count_words-1)
-                    rem = space % (count_words-1)
-                    temp = ''
-                    for j in range(start, i):
-                        temp += words[j] + (' ' * (sep+2) if j < start+rem else ' ' * (sep+1))
-                    res.append(temp[: maxWidth])
-                # reset and start a new line
-                start = i
-                count_chars = 0
-                count_words = 0
-            # count the current word
-            count_words += 1
-            count_chars += len(words[i])
-        # last line left justified
-        space = maxWidth - count_chars - (count_words - 1)
-        res.append(' '.join(words[start:]) + ' '*space)
-        return res
+        def add_word(spaces, curr):
+            spaces += maxWidth - (curr + spaces)            # add remaining spaces
+            if len(line) > 1:                               # contain multiple words
+                mid, last = divmod(spaces, len(line) - 1)   # get expected len of spaces 
+                if not last:                                # spaces divided evenly
+                    res = (" " * mid).join(line)
+                else:                                       # spaces divided oddly
+                    res = []
+                    for word in line:                       # reiterate words in line to add spaces
+                        add = 0
+                        if last:                            # check if we have more extra spaces
+                            add += 1                        # add + 1 until we exceed all 'last' spaces
+                            last -= 1                       # decrement accordingly
+                        res.append(word)                    # add word
+                        res.append(" " * (mid + add))       # add space
+                    res.pop()                               # remove last extra space
+                    res = "".join(res)
+                lines.append(res)
+            else:                                           # contain single word
+                lines.append("".join(line) + " " * spaces)
+        
+        lines = []
+        line = []
+        curr = 0
+        spaces = 0
+        for word in words:
+            if not line:                                    # start of a sentence
+                line.append(word)
+                curr += len(word)
+            elif curr + spaces + 1 + len(word) > maxWidth:  # new line
+                add_word(spaces, curr)
+                line = [word]                               # init to start of a sentence
+                curr = len(word)
+                spaces = 0
+            else:                                           # continue with curr line
+                line.append(word)
+                curr += len(word)
+                spaces += 1
+        if line:                                            # add last line
+            final_line = [" ".join(line)]
+            final_line.append(" " * (maxWidth - (curr + spaces)))
+            lines.append("".join(final_line))
+        return lines
